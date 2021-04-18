0d06a8d35a9cbafee467c8713f0553eff88cb027
imtony3579 <173010031@iitb.ac.in>
2021-04-18 23:11:00 +0530

    JS_objects

--- Javascript_u/script.js
+++ Javascript_u/script.js
@@ -1,8 +1,21 @@
-var newArray = ['a', [2, 3, 5, 6], 'b'];
-
-for (var index in newArray) {
-    console.log(index)
-    if (newArray[index].constructor === Array) {
-        console.log(newArray[index]);
+var person = {
+    name: "Jhon Smith",
+    age: 28,
+    skills: ["Cpp", "python", "Java script", "etc"],
+    change_name: function(new_name) {
+        this.name = new_name;
+        return this;
+    },
+    change_age: function(age_number) {
+        this.age = age_number;
+        return this;
     }
+};
+
+person.change_name("Rimaru Tempest").change_age(123);
+
+document.querySelector('#test').innerHTML = person.name + " " + person.age + " have skill " + person.skills.join(", ");
+
+for (var key in person) {
+    console.log(key, person[key].constructor);
 }
